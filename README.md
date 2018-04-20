02cd1d8fe3624d71bbed03c92f6987e0f666b31e
Richard Prillwitz <richard.prillwitz@myracloud.com>
2018-04-20 16:03:30 +0200

    battery script

--- README.md
+++ README.md
@@ -2,13 +2,28 @@
 
 ## time
 Shows the time in different colors, depending on the current time (hour)
+
 ### Configuration
 ```
 make i3b_time
+sudo ln -s $(pwd)/i3b_time /usr/share/i3blocks/i3b_time
 ```
 in your i3blocks.conf:
 ```
 # Date Time
 [i3b_time]
 interval=1
+```
+
+## battery
+Thinkpads sometimes have multiple batteries
+```
+make i3b_battery
+sudo ln -s $(pwd)/i3b_battery /usr/share/i3blocks/i3b_battery
+```
+in your i3blocks.conf:
+```
+# Battery
+[i3b_battery]
+interval=30
 ```
--- battery/main.go
+++ battery/main.go
@@ -1,30 +1,72 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os/exec"
+	"strconv"
+	"strings"
 )
 
 var (
-	iconFull    = ""
-	iconGood    = ""
-	iconOkay    = ""
-	iconPoor    = ""
-	iconLow     = ""
-	iconVeryLow = ""
-	cFull       = "#FF0000"
-	cGood       = "#FF0000"
-	cOkay       = "#FF0000"
-	cPoor       = "#FF6600"
-	cLow        = "#FF0000"
-	cVeryLow    = "#FF0000"
+	iconCharging = ""
+	iconFull     = "" // >90
+	iconGood     = "" // >80
+	iconOkay     = "" // >50
+	iconPoor     = "" // >20
+	iconLow      = "" // <20
+	cFull        = "#05F900"
+	cGood        = "#BFF929"
+	cOkay        = "#F9F420"
+	cPoor        = "#F9C11A"
+	cLow         = "#FF6600"
 )
 
 func main() {
 	out, err := exec.Command("acpi", "b").Output()
 	if err != nil {
 		log.Fatal(err)
+		panic(err)
 	}
-	fmt.Println(string(out))
+
+	lines := bytes.Split(out[:len(out)-1], []byte("\n"))
+
+	var fullText string
+	var shortText string
+	var color string
+
+	for _, bat := range lines {
+		values := bytes.Split(bat, []byte(":"))
+		batPercent := strings.Trim(string(values[1][len(values[1])-4:]), " %")
+		percent, err := strconv.Atoi(batPercent)
+		if err != nil {
+			continue
+		}
+		icon, color := iconAndColor(percent)
+
+		fullText = fmt.Sprintf("%s <span foreground=\"%s\">%s %s%s</span>", fullText, color, icon, batPercent, "%")
+	}
+
+	fmt.Println(fullText)
+	fmt.Println(shortText)
+	fmt.Println(color)
+}
+
+//
+// iconAndColor returns the icon and the color for the given percent value
+//
+func iconAndColor(percent int) (string, string) {
+	switch {
+	case percent > 90:
+		return iconFull, cFull
+	case percent > 80:
+		return iconGood, cGood
+	case percent > 50:
+		return iconOkay, cOkay
+	case percent > 20:
+		return iconPoor, cPoor
+	default:
+		return iconLow, cLow
+	}
 }
