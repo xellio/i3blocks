8ef6bf561ae41ac71de85d2022ba052d15709170
Richard Prillwitz <richard.prillwitz@myracloud.com>
2018-05-07 10:19:52 +0200

    little cleanup;

--- battery_information/main.go
+++ battery_information/main.go
@@ -24,17 +24,9 @@ func main() {
 
 	var output string
 
-	for _, bi := range information.BatteryInformation {
-		output = fmt.Sprintf("%s\nBattery %d: %s\nLevel: %d", output, bi.Number, bi.Status, bi.Level)
-	}
-
-	for _, ai := range information.AdapterInformation {
-		output = fmt.Sprintf("%s\nAdapter %d: %s", output, ai.Number, ai.Status)
-	}
-
-	for _, ti := range information.ThermalInformation {
-		output = fmt.Sprintf("%s\nThermal %d: %.2f%s", output, ti.Number, ti.Degree, ti.Unit)
-	}
+	output = appendBatteryInformation(output, information.BatteryInformation)
+	output = appendAdapterInformation(output, information.AdapterInformation)
+	output = appendThermalInformation(output, information.ThermalInformation)
 
 	err = notify(path,
 		"--no-buttons",
@@ -47,6 +39,42 @@ func main() {
 		panic(err)
 	}
 
+}
+
+func appendBatteryInformation(output string, information []*acpi.BatteryInformation) string {
+
+	if len(information) > 1 {
+		for _, bi := range information {
+			if bi.Status != "Unknown" {
+				output = fmt.Sprintf("%s\nBattery %d: %s\nLevel: %d", output, bi.Number, bi.Status, bi.Level)
+			} else {
+				output = fmt.Sprintf("%s\nBattery %d\nLevel: %d", output, bi.Number, bi.Level)
+			}
+		}
+		return output + "\n"
+	}
+
+	return fmt.Sprintf("%s\nLevel: %d\n", output, information[0].Level)
+}
+
+func appendAdapterInformation(output string, information []*acpi.AdapterInformation) string {
+	if len(information) > 1 {
+		for _, ai := range information {
+			output = fmt.Sprintf("%s\nAdapter %d: %s", output, ai.Number, ai.Status)
+		}
+		return output + "\n"
+	}
+	return fmt.Sprintf("%s\nAdapter: %s\n", output, information[0].Status)
+}
+
+func appendThermalInformation(output string, information []*acpi.ThermalInformation) string {
+	if len(information) > 1 {
+		for _, ti := range information {
+			output = fmt.Sprintf("%s\nThermal %d: %.2f%s", output, ti.Number, ti.Degree, ti.Unit)
+		}
+		return output + "\n"
+	}
+	return fmt.Sprintf("%s\nThermal: %.2f%s", output, information[0].Degree, information[0].Unit)
 }
 
 func notify(path string, args ...string) error {
